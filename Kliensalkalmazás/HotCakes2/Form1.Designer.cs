db7cc6e430a06bf8c6bd401b414a4f94c0295b2d
artifox2023 <131253878+artifox2023@users.noreply.github.com>
2023-05-03 12:57:17 +0200

    Add files via upload

---
+++ Kliensalkalmazás/HotCakes2/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace HotCakes2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TermekekDGV = new System.Windows.Forms.DataGridView();
+            this.buttonEditPrice = new System.Windows.Forms.Button();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonEditDescription = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textboxDescription = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TermekekDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TermekekDGV
+            // 
+            this.TermekekDGV.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TermekekDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TermekekDGV.Location = new System.Drawing.Point(12, 12);
+            this.TermekekDGV.Name = "TermekekDGV";
+            this.TermekekDGV.RowHeadersWidth = 51;
+            this.TermekekDGV.RowTemplate.Height = 24;
+            this.TermekekDGV.Size = new System.Drawing.Size(893, 403);
+            this.TermekekDGV.TabIndex = 0;
+            this.TermekekDGV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TermekekDGV_CellClick);
+            this.TermekekDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.TermekekDGV.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.TermekekDGV_CellLeave);
+            // 
+            // buttonEditPrice
+            // 
+            this.buttonEditPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonEditPrice.Location = new System.Drawing.Point(175, 479);
+            this.buttonEditPrice.Name = "buttonEditPrice";
+            this.buttonEditPrice.Size = new System.Drawing.Size(144, 51);
+            this.buttonEditPrice.TabIndex = 1;
+            this.buttonEditPrice.Text = "Ár szerkesztése";
+            this.buttonEditPrice.UseVisualStyleBackColor = true;
+            this.buttonEditPrice.Click += new System.EventHandler(this.Szerkesztés_Click);
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(21, 494);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPrice.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(9, 453);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Új termékár megadása:";
+            // 
+            // buttonEditDescription
+            // 
+            this.buttonEditDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonEditDescription.Location = new System.Drawing.Point(727, 479);
+            this.buttonEditDescription.Name = "buttonEditDescription";
+            this.buttonEditDescription.Size = new System.Drawing.Size(178, 51);
+            this.buttonEditDescription.TabIndex = 5;
+            this.buttonEditDescription.Text = "Leírás szerkesztése";
+            this.buttonEditDescription.UseVisualStyleBackColor = true;
+            this.buttonEditDescription.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(424, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Új termékleírás megadása:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(127, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ft";
+            // 
+            // textboxDescription
+            // 
+            this.textboxDescription.Location = new System.Drawing.Point(418, 473);
+            this.textboxDescription.Name = "textboxDescription";
+            this.textboxDescription.Size = new System.Drawing.Size(303, 96);
+            this.textboxDescription.TabIndex = 9;
+            this.textboxDescription.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 581);
+            this.Controls.Add(this.textboxDescription);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonEditDescription);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.buttonEditPrice);
+            this.Controls.Add(this.TermekekDGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.TermekekDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView TermekekDGV;
+        private System.Windows.Forms.Button buttonEditPrice;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonEditDescription;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox textboxDescription;
+    }
+}
+
---
+++ Kliensalkalmazás/HotCakes2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Client;
+using Newtonsoft.Json;
+using Hotcakes.Commerce.Catalog;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json.Linq;
+
+namespace HotCakes2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetData();
+        }
+
+        
+        //A termékek és adatinak betöltése a datagridviewba
+        public void GetData()
+        {
+            string url = "http://20.234.113.211:8093/";
+            string key = "1-cebeaa9d-f647-4750-9821-26f097f3d6d4";
+
+
+
+            Api proxy = new Api(url, key);
+
+            ApiResponse<List<ProductDTO>> response = proxy.ProductsFindAll();
+
+            string json = JsonConvert.SerializeObject(response);
+
+            ApiResponse<List<ProductDTO>> deserializedResponse = JsonConvert.DeserializeObject<ApiResponse<List<ProductDTO>>>(json);
+
+            DataTable dt = new DataTable();
+            
+            dt.Columns.Add("ProductName", typeof(string));
+            dt.Columns.Add("Sku", typeof(string));
+            dt.Columns.Add("SitePrice", typeof(string));
+            dt.Columns.Add("Description", typeof(string));
+            dt.Columns.Add("CreationDateUtc", typeof(DateTime));
+            dt.Columns.Add("Bvin", typeof(string));
+
+            foreach (ProductDTO item in deserializedResponse.Content)
+            {
+                dt.Rows.Add( item.ProductName, item.Sku, item.SitePrice, item.LongDescription, item.CreationDateUtc, item.Bvin);
+            }
+            
+            TermekekDGV.DataSource = dt;
+
+            
+
+        }
+
+        //Az ár megváltoztatásához függvény
+        public void ChangePrice()
+        {
+            string url = "http://20.234.113.211:8093/";
+            string key = "1-cebeaa9d-f647-4750-9821-26f097f3d6d4";
+
+
+
+            Api proxy = new Api(url, key);
+
+            //A jelenlegi sor ID-nak meghatározása
+            int rowIndex = TermekekDGV.CurrentCell.RowIndex;
+            
+            var productId = TermekekDGV.Rows[rowIndex].Cells["Bvin"].Value.ToString();
+
+            var product = proxy.ProductsFind(productId).Content;
+
+            //A kiválasztott termék leírásának felülírása
+            product.SitePrice = int.Parse(textBoxPrice.Text);
+            ApiResponse<ProductDTO> response = proxy.ProductsUpdate(product);
+            textBoxPrice.Clear();
+
+            //A végén a frissített adatok betöltése
+            GetData();
+           
+        }
+
+        //A leírás megváltoztatásához függvény
+        public void ChangeDescription()
+        {
+            string url = "http://20.234.113.211:8093/";
+            string key = "1-cebeaa9d-f647-4750-9821-26f097f3d6d4";
+
+
+
+            Api proxy = new Api(url, key);
+
+            //A jelenlegi sor ID-nak meghatározása
+            int rowIndex = TermekekDGV.CurrentCell.RowIndex;
+
+            var productId = TermekekDGV.Rows[rowIndex].Cells["Bvin"].Value.ToString();
+
+            var product = proxy.ProductsFind(productId).Content;
+
+            //A kiválasztott termék leírásának felülírása
+            product.LongDescription = textboxDescription.Text;
+
+            ApiResponse<ProductDTO> response = proxy.ProductsUpdate(product);
+            textboxDescription.Clear();
+
+            //A végén a frissített adatok betöltése
+            GetData();
+
+            
+        }
+
+        //Ha a felhasználó az 'ár megváltoztatása' nevű gombra kattint
+        private void Szerkesztés_Click(object sender, EventArgs e)
+        {
+
+            //Ellenőrzés, hogy nem üres-e az ennek megfelelő textbox (üres értéket nem lehet megadni)
+            if (string.IsNullOrEmpty(textBoxPrice.Text))
+            {
+                MessageBox.Show("Kérjük adjon meg értéket!", "Nem adott meg új árat!");
+            }
+            else
+            {
+                //Ellenőrzés, hogy számértéket adott-e meg a felhasználó
+                if (!int.TryParse(textBoxPrice.Text, out int price))
+                {
+                    MessageBox.Show("Kérjük, számértéket adjon meg az árnak!", "Érvénytelen érték!");
+                    textBoxPrice.Focus();
+                    return;
+                }
+
+                //Megerősítés a változtatás véghezvitele előtt
+                DialogResult result = MessageBox.Show("Biztos meg akarod változtatni az árat?", "Megerősítés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    ChangePrice();
+                    MessageBox.Show("A változtatás sikeres volt!", "Sikeres módosítás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+            
+        }
+
+        //Ha a felhasználó az 'Leírás szerkesztése' nevű gombra kattint
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            //Ellenőrzés, hogy nem üres-e az ennek megfelelő textbox (üres értéket nem lehet megadni)
+            if (string.IsNullOrEmpty(textboxDescription.Text))
+            {
+                MessageBox.Show("Kérjük adjon meg termékleírást!", "Nem adott meg új leírást!");
+                textboxDescription.Focus();
+            }
+            else
+            {
+                //Megerősítés a változtatás véghezvitele előtt
+                DialogResult result = MessageBox.Show("Biztos meg akarod változtatni a leírást?", "Megerősítés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    ChangeDescription();
+                    MessageBox.Show("A változtatás sikeres volt!", "Sikeres módosítás", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+
+        }
+
+        //jelenlegi sor kiemelése
+        private void TermekekDGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Elmentjük a kattintott sor indexét
+            int sorIndex = e.RowIndex;
+
+            // Ellenőrizzük, hogy valóban kattintottak-e sorra
+            if (sorIndex >= 0 && sorIndex < TermekekDGV.Rows.Count)
+            {
+                // Kijelöljük a korábban kattintott sorokat
+                foreach (DataGridViewRow sor in TermekekDGV.Rows)
+                {
+                    sor.Selected = false;
+                }
+
+                // Kijelöljük a kattintott sort és megváltoztatjuk a háttérszínét
+                DataGridViewRow kattintottSor = TermekekDGV.Rows[sorIndex];
+                kattintottSor.Selected = true;
+                TermekekDGV.DefaultCellStyle.SelectionBackColor = Color.Green;
+            }
+        }
+
+
+        private void TermekekDGV_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
