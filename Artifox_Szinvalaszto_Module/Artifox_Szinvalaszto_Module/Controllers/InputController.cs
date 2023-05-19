6429a540cb9f7442a3bb4a2dbeac287d86bc8c1d
Márton András Takács <marton.takacs2@stud.uni-corvinus.hu>
2023-05-19 19:12:09 +0200

    Picture upload és colorpicker done, kinézeten lehet még javítani

---
+++ Artifox_Szinvalaszto_Module/Artifox_Szinvalaszto_Module/Controllers/InputController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+
+namespace Dnn.SzinvalasztoArtifox_Szinvalaszto_Module.Controllers
+{
+    [DnnHandleError]
+    public class InputController : DnnController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
