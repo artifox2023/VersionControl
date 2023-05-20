eea828ecebb7b25f5382d2f65611d35c96db9eb4
Márton András Takács <marton.takacs2@stud.uni-corvinus.hu>
2023-05-20 17:14:09 +0200

    Nagyon basic modul, működik, elvileg a kép felbontása sem jelent gondot

---
+++ Artifox_Szinvalaszto_Module/Artifox_Szinvalaszto_Module/Models/Image.cs
@@ -0,0 +1,19 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnn.SzinvalasztoArtifox_Szinvalaszto_Module.Models
+{
+    [TableName("Artifox_Szinvalaszto_Module_Image")]
+    [PrimaryKey(nameof(ImageId), AutoIncrement = true)]
+    [Scope("ModuleId")]
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; }
+        public int UserId { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ Artifox_Szinvalaszto_Module/Artifox_Szinvalaszto_Module/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnn.SzinvalasztoArtifox_Szinvalaszto_Module.Models
+{
+    [TableName("Artifox_Szinvalaszto_Module_Product")]
+    [PrimaryKey(nameof(ProductId), AutoIncrement = true)]
+    [Scope("ModuleId")]
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int RGB_calculated { get; set; }
+    }
+}
